46d4148571f11dc28eaa445fe47beeea211d7bfb
ofer <ofergavri@gmail.com>
2023-02-24 22:12:38 +0200

    done header + hero  components

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -3,12 +3,46 @@ import React from 'react';
 import { AiFillGithub, AiFillInstagram, AiFillLinkedin } from 'react-icons/ai';
 import { DiCssdeck } from 'react-icons/di';
 
-import { Container, Div1, Div2, Div3, NavLink, SocialIcons } from './HeaderStyles';
+import { Container, Div1, Div2, Div3, NavLink, SocialIcons, Span } from './HeaderStyles';
 
-const Header = () =>  (
-  <div>
-    Header
-  </div>
-);
+const Header = () => (
+  <Container>
+    <Div1>
+      <Link href={'/'}>
+        <a style={{ display: 'flex', alignItems: 'center', color: 'white', marginBottom: '20px' }}>
+          <DiCssdeck size={'3rem'} /> <Span>Portfolio</Span>
+        </a>
+      </Link>
+    </Div1>
+    <Div2>
+      <li>
+        <Link href="#projects">
+          <NavLink>Projects</NavLink>
+        </Link>
+      </li>
+      <li>
+        <Link href="#tech">
+          <NavLink>Technologies</NavLink>
+        </Link>
+      </li>
+      <li>
+        <Link href="#about">
+          <NavLink>About</NavLink>
+        </Link>
+      </li>
+    </Div2>
+    <Div3>
+      <SocialIcons href='https://github.com/oferGavrilov'>
+        <AiFillGithub size={'3rem'} />
+      </SocialIcons>
+      <SocialIcons href='https://www.linkedin.com/in/ofergavrilov/'>
+        <AiFillLinkedin size={'3rem'} />
+      </SocialIcons>
+      <SocialIcons href='https://github.com/oferGavrilov'>
+        <AiFillInstagram size={'3rem'} />
+      </SocialIcons>
+    </Div3>
+  </Container>
+)
 
 export default Header;
--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -1,13 +1,22 @@
-import React from 'react';
+import React from 'react'
 
-import { Section, SectionText, SectionTitle } from '../../styles/GlobalComponents';
-import Button from '../../styles/GlobalComponents/Button';
-import { LeftSection } from './HeroStyles';
+import { Section, SectionText, SectionTitle } from '../../styles/GlobalComponents'
+import Button from '../../styles/GlobalComponents/Button'
+import { LeftSection } from './HeroStyles'
 
-const Hero = (props) => (
-  <div>
-    Hero
-  </div>
-);
+const Hero = () => (
+  <Section row nopadding>
+    <LeftSection>
+      <SectionTitle main center>
+        Welcome To <br />
+        My Personal Portfolio
+      </SectionTitle>
+      <SectionText>
+        My name is Ofer Gavri and i'm a Full-Stack developer and my purpose is to elevate Websites to the Next Level !
+      </SectionText>
+      <Button onClick={() => window.location = 'https://github.com/oferGavrilov'}>Learn More</Button>
+    </LeftSection>
+  </Section>
+)
 
 export default Hero;
