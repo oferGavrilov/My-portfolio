0ed39d7ed1986fbac6dd7fa6d35803cf951d08c5
ofer <ofergavri@gmail.com>
2023-05-06 12:25:10 +0300

    added google analytics

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,9 +4,20 @@ import { type AppProps } from 'next/app'
 import '../styles/Loader.css'
 import '../styles/buttons.css'
 import React from 'react'
+import Script from 'next/script'
 export default function App ({ Component, pageProps }: AppProps): JSX.Element {
   return (
     <Theme>
+      <Script async strategy='afterInteractive' src='https://www.googletagmanager.com/gtag/js?id=G-CB1BSL3NER' />
+      <Script id='google-analytics' strategy='afterInteractive'>
+        {
+          `window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+
+        gtag('config', 'G-CB1BSL3NER');
+      `}
+      </Script>
       <Component {...pageProps} />
     </Theme>
   )
